a099ac113795967e182ce60d2e39524ba639cd38
ronnie012 <ronnie.247365.1@gmail.com>
2025-07-28 05:15:59 +0600

    Enhanced all management pages in admin dashboard.

--- src/pages/Dashboard/Admin/ManageTransactions.jsx
+++ src/pages/Dashboard/Admin/ManageTransactions.jsx
@@ -95,6 +95,7 @@ const ManageTransactions = () => {
                   <div role="status" className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]">
                     <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
                   </div>
+                  <p className="text-red-600 text-5xl font-bold mt-4">Loading transactions...</p>
                 </td>
               </tr>
             ) : isError ? (
